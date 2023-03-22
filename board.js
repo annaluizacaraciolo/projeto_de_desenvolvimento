fe0fb723dffb83936c62d7a1ccd1e80c4ca9b8c9
Anna Luiza Caraciolo <55456092+annaluizacaraciolo@users.noreply.github.com>
2023-03-22 16:52:32 -0300

    Added the speech to the board

--- board.js
+++ board.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector(".modal");
 const trigger = document.querySelector(".adicionar");
 const closeButton = document.querySelector("#fechar");
+const confirmButton = document.querySelector("#confirmar")
 
 function toggleModal() {
     modal.classList.toggle("show-modal");
@@ -16,7 +17,13 @@ function cleanText() {
     texto.value = null;
 }
 
+function confirmText() {
+    document.getElementById("texto-capturado").innerText = texto.value;
+}
+
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+confirmButton.addEventListener("click", confirmText);
+confirmButton.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", cleanText);
